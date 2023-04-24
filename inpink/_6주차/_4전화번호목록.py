2d2b35d891049c4ac5d8f19267aa4bd0f0ad363b
inpink <108166692+inpink@users.noreply.github.com>
2023-04-24 19:10:22 +0900

    Docs : 전화번호목록

--- inpink/_6주차/_4전화번호목록.py
+++ inpink/_6주차/_4전화번호목록.py
@@ -1,1 +1,15 @@
-
+def solution(phone_book):
+    answer = True
+    d={}
+    
+    for i in phone_book:
+        d[i]=True
+    
+    for i in phone_book:
+        tmp=""
+        for j in i:
+            tmp+=j
+            if tmp in d:
+                if i!=tmp:
+                    answer=False
+    return answer
